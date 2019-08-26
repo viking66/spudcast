b5833b00afdb97aa6c17b05ef9b38a7a080095d4
Jason Davidson <jad658@gmail.com>
2019-08-25 19:28:31 -0700

    Use backwards compatible hedgehog code

--- test/Prop.hs
+++ test/Prop.hs
@@ -1,8 +1,18 @@
 module Main where
 
-import Hedgehog.Main
+import Control.Monad (unless)
+import System.Exit (exitFailure)
+import System.IO (BufferMode (LineBuffering), hSetBuffering, stderr, stdout)
 
 import qualified Spudcast.API.TypesProp as Spudcast.API.TypesProp
+
+defaultMain :: [IO Bool] -> IO ()
+defaultMain tests = do
+  hSetBuffering stdout LineBuffering
+  hSetBuffering stderr LineBuffering
+  result <- and <$> sequence tests
+  unless result
+    exitFailure
 
 main :: IO ()
 main = defaultMain
